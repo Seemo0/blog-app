353dc8c8500efbf6c8a3c855320d45ffc8b69dd5
Seemo0 <medouallal08@gmail.com>
2023-01-28 15:59:44 +0100

    ..

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Navigate } from "react-router-dom";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image"],
+    ["clean"],
+  ],
+};
+
 
 export default function CreatePost() {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+
+
+  // if (redirect) {
+  //   return <Navigate to={"/"} />;
+  // }
+
   return (
-    <div>CreatePost</div>
-  )
+    <form>
+      <input
+        type="title"
+        placeholder={"Title"}
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder={"Summary"}
+        value={summary}
+        onChange={(ev) => setSummary(ev.target.value)}
+      />
+      <input type="file" onChange={(ev) => setFiles(ev.target.files)} />
+      {/* <Editor value={content} onChange={setContent} /> */}
+      <ReactQuill
+        value={content}
+        onChange={(newValue) => setContent(newValue)}
+        modules={modules}
+      />
+      <button style={{ marginTop: "5px" }}>Create post</button>
+    </form>
+  );
 }
